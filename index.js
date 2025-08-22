b7aa5758c46b3a12b9923e57cb03d06778481894
Ivan Yeung <ivanyeung0610@gmail.com>
2025-08-22 15:33:22 -0400

    Implement human choice function
    Created the getHumanChoice function and tested the funtionality.

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+/* Psuedocode:
+ * gets user input (0, 1, or 2)
+ * return rock if 0, paper if 1, and scissor otherwise
+ */
+function getHumanChoice() {
+    let choice = prompt("Type 0 for rock, 1 for paper, 2 for scissors.");
+    if (choice === 0) {
+        return "rock";
+    } else if (choice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getHumanChoice());
